c219e1de1057234aa52e63fdba2d724c7f96e98a
BuildTools <unconfigured@null.spigotmc.org>
2016-09-15 18:05:57 -0700

    Files

---
+++ Test/player.gd
@@ -0,0 +1,14 @@
+extends KinematicBody
+var view_sensitivity = 0.3
+var yaw = 0
+var pitch = 0
+
+func _input(ie):
+	pass
+	
+funch _fixed_process(delta):
+	pass
+	
+func _ready():
+	set_fixed_process(true)
+	set_process_input(true)
