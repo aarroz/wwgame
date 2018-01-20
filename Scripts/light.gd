c52ddab1c72398806d434d2a4c13db3516c187de
aarroz <aarroz@protonmail.com>
2018-01-19 22:47:49 -0800

    Changed a bunch of things for Godot 3.0

--- Scripts/light.gd
+++
@@ -1,5 +0,0 @@
-extends Light
-
-func _input(event):
-	if Input.is_key_pressed(KEY_F):
-		is_enabled(false)
---
+++ assets/Scripts/mainmenu.gd
@@ -0,0 +1,16 @@
+extends Node
+
+var global = "/root/global"
+
+
+func _on_Play_pressed():
+	get_node(global).setScene("res://assets/Scenes/level1.tscn")
+
+func _on_Settings_pressed():
+	get_node("Settings").show()
+
+func _on_Quit_pressed():
+	get_tree().quit()
+
+func _on_Back_pressed():
+	get_node("Settings").hide()
