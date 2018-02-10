8b0265e56467ab0f3789135cc13ca8f62289bd2b
aarroz <aarroz@protonmail.com>
2018-02-09 16:42:44 -0800

    Added menu_settings.tscn

--- assets/Scripts/Settings.gd
+++ assets/Scripts/Settings.gd
@@ -13,3 +13,7 @@ func _ready():
 #	# Called every frame. Delta is time since last frame.
 #	# Update game logic here.
 #	pass
+
+
+func _on_OptionButton_item_selected( ID ):
+	pass # replace with function body
