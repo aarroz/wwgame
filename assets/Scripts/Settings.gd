e42e87a3a44bba6dd909a2520bc38d89759ecd19
Aaron <arroz@protonmail.com>
2019-06-05 20:33:11 -0700

    Updated to Godot 3.1 and latest simplefps

--- assets/Scripts/Settings.gd
+++ assets/Scripts/Settings.gd
@@ -1,4 +1,4 @@
-extends Panel
+extends Control
 
 onready var aliasing = get_node("Anti-Aliasing/OptionButton")
 onready var ssao = get_node("SSAO/OptionButton")
@@ -19,29 +19,29 @@ func _on_VSync_button_toggled( button_pressed ):
 		OS.set_use_vsync(true)
 
 
-func _on_OptionButton_item_selected( ID ):
-	OptionButton.get_selected_id()
-	if OptionButton.get_item_id(-1):
-		VisualServer.viewport_set_msaa(0)
-	if OptionButton._select_int(0):
-		VisualServer.viewport_set_msaa(1)
-	if OptionButton._select_int(1):
-		VisualServer.viewport_set_msaa(2)
-	if OptionButton._select_int(2):
-		VisualServer.viewport_set_msaa(3)
-	if OptionButton._select_int(3):
-		VisualServer.viewport_set_msaa(4)
+#func _on_OptionButton_item_selected( ID ):
+#	OptionButton.get_selected_id()
+#	if OptionButton.get_item_id(-1):
+#		VisualServer.viewport_set_msaa(0)
+#	if OptionButton._select_int(0):
+#		VisualServer.viewport_set_msaa(1)
+#	if OptionButton._select_int(1):
+#		VisualServer.viewport_set_msaa(2)
+#	if OptionButton._select_int(2):
+#		VisualServer.viewport_set_msaa(3)
+#	if OptionButton._select_int(3):
+#		VisualServer.viewport_set_msaa(4)
 
 
-func _on_BloomButton_toggled( button_pressed ):
-	if Environment.is_glow_enabled():
-		Environment.set_glow_enabled(false)
-	else:
-		Environment.set_glow_enabled(true)
+#func _on_BloomButton_toggled( button_pressed ):
+#	if Environment.is_glow_enabled():
+#		Environment.set_glow_enabled(false)
+#	else:
+#		Environment.set_glow_enabled(true)
 
 
-func _on_SSRButton_toggled( button_pressed ):
-	if Environment.is_ssao_enabled():
-		Environment.set_ssao_enabled(false)
-	else:
-		Environment.set_ssao_enabled(true)
+#func _on_SSRButton_toggled( button_pressed ):
+#	if Environment.is_ssao_enabled():
+#		Environment.set_ssao_enabled(false)
+#	else:
+#		Environment.set_ssao_enabled(true)
