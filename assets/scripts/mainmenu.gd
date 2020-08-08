841e1275c1a2e223e4c173b24e3779e41868ca09
aarroz <aarroz@users.noreply.github.com>
2020-08-08 15:27:09 -0700

    Added level select menu

--- assets/scripts/mainmenu.gd
+++ assets/scripts/mainmenu.gd
@@ -4,7 +4,8 @@ var global = "/root/global"
 
 
 func _on_Play_pressed():
-	get_node(global).setScene("res://assets/scenes/level_intro_prototype.tscn")
+	get_node("Level_Select").show()
+	#get_node(global).setScene("res://assets/scenes/level_intro_prototype.tscn")
 	#get_node(global).setScene("res://assets/Scenes/level1.tscn")
 
 func _on_Settings_pressed():
@@ -15,3 +16,15 @@ func _on_Quit_pressed():
 
 func _on_Back_pressed():
 	get_node("Settings").hide()
+
+
+func _on_Option_1_pressed():
+	get_node(global).setScene("res://assets/scenes/level1.tscn")
+
+
+func _on_Option_2_pressed():
+	get_node(global).setScene("res://assets/scenes/level_intro_prototype.tscn")
+
+
+func _on_Option_3_pressed():
+	get_node(global).setScene("res://assets/scenes/prototype_box.tscn")
