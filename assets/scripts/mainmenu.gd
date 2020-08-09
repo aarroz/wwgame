dd75b0b52af05eb611767c30ad63085c1b6f20bb
aarroz <aarroz@users.noreply.github.com>
2020-08-08 22:23:56 -0700

    Added puzzles, along with tutorial puzzle

--- assets/scripts/mainmenu.gd
+++ assets/scripts/mainmenu.gd
@@ -1,12 +1,11 @@
 extends Node
 
 var global = "/root/global"
-
+onready var level_tutorial = preload("res://assets/scenes/puzzles/level_tutorial.tscn")
 
 func _on_Play_pressed():
-	get_node("Level_Select").show()
-	#get_node(global).setScene("res://assets/scenes/level_intro_prototype.tscn")
-	#get_node(global).setScene("res://assets/Scenes/level1.tscn")
+	#get_node("Level_Select").show()
+	get_node(global).setScene("res://assets/scenes/puzzles/level_tutorial.tscn")
 
 func _on_Settings_pressed():
 	get_node("Settings").show()
